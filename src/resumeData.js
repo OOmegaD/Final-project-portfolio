9b4eeef67ba567363c71cd6cfeae767029bf2713
OOmegaD <fthomas172@gmail.com>
2021-05-26 13:50:20 -0400

    Add files via upload

---
+++ src/resumeData.js
@@ -0,0 +1,33 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/OOmegaD/",
+    "name": "Dorian Thomas",
+    "role": "Front-End Developer",
+    "linkedinId":" @Dorian Thomas",
+    "skypeid": "@Dorian Thomas",
+    "roleDescription": " Curious about life.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/dorianthomas",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/OOmegaD",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/oomegaD",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I'm a computer science student curious about life",
+    "address":"Bronx, New York",
+    "website":"https://github.com/OOmegaD",
+    "education":[
+     
+    ]
+  }
+  
+  export default resumeData
